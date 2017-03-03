72f1aaafae1216aecdfbbbc865b68664e42e1312
Guillermo <guille.mca@hotmail.com>
2017-03-03 17:28:50 +0100

    finalizado imprimir titutlos y opciones de preguntaS

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,7 +1,17 @@
 var formContainer = null;
-
+var nota = 0.0;
+var preguntasSelect = [];
 window.onload = function(){ 
-	formContainer = document.getElementById('myform');
+	//CORREGIR al apretar el botón
+	formContainer=document.getElementById('myform');
+	formContainer.onsubmit=function(){
+
+	inicializar();
+ 	presentarNota();
+ 	return false;
+
+ }
+
 	//LEER XML de xml/preguntas.xml
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
@@ -32,6 +42,18 @@ function gestionarXml(dadesXml){
 				imprimirTituloPregunta(i, xmlDoc);
 				imprimirCajaText(numeroCajaTexto, xmlDoc);
 				numeroCajaTexto++;
+				break;
+			case "checkbox":
+				imprimirTituloPregunta(i, xmlDoc);
+				imprimirCheckBox(i, xmlDoc);
+				break;
+			case "radio":
+				imprimirTituloPregunta(i, xmlDoc);
+				imprimirRadioButton(i, xmlDoc);
+				break;
+			case "select multiple":
+				imprimirTituloPregunta(i, xmlDoc);
+				imprimirSelectMultiple(i, xmlDoc);
 				break;
 		}
 	}
@@ -68,11 +90,52 @@ function imprimirCajaText(numeroCajaTexto, xmlDoc) {
 	formContainer.appendChild(cajaTexto);
 }
 
-function imprimirBotonCorregir() {
-	var botonCorregir = document.createElement("input");
-	botonCorregir.type = "submit";
-	botonCorregir.value = "Corregir";
-	formContainer.appendChild(botonCorregir);
+
+function imprimirCheckBox(i, xmlDoc) {
+	var numOpciones = xmlDoc.getElementsByTagName('question')[i].getElementsByTagName('option').length;
+	var opt = xmlDoc.getElementsByTagName('question')[i].getElementsByTagName('option');
+	for (j = 0; j < numOpciones; j++) {
+		var label = document.createElement("label");
+		var input = document.createElement("input");
+		label.innerHTML=opt[j].innerHTML;
+		input.type="checkbox";
+		input.name="preg"+i;
+		input.id="preg"+i+"ans"+j;
+		formContainer.appendChild(input);
+		formContainer.appendChild(label);
+		formContainer.appendChild(document.createElement("br"));
+	}
+}
+
+function imprimirRadioButton(i, xmlDoc) {
+	var numOpciones = xmlDoc.getElementsByTagName('question')[i].getElementsByTagName('option').length;
+	var opt = xmlDoc.getElementsByTagName('question')[i].getElementsByTagName('option');
+	for (j = 0; j < numOpciones; j++) {
+		var input = document.createElement("input");
+		var answerTitle = opt[j].innerHTML;
+		var span = document.createElement("span");
+		span.innerHTML = answerTitle;
+		input.type="radio";
+		input.name="preg"+i;
+		input.id="preg"+i+"ans"+j;
+		formContainer.appendChild(input);
+		formContainer.appendChild(span);
+		formContainer.appendChild(document.createElement("br"));
+	}	
+}
+
+function imprimirSelectMultiple(i, xmlDoc) {
+	var numOpciones = xmlDoc.getElementsByTagName('question')[i].getElementsByTagName('option').length;
+	var opt = xmlDoc.getElementsByTagName('question')[i].getElementsByTagName('option');
+	var selectMultiple = document.createElement("select");
+	selectMultiple.multiple="true";
+	for (j = 0; j < numOpciones; j++) {
+		var answerTitle = opt[j].innerHTML;
+		var option = document.createElement("option");
+		option.innerHTML = answerTitle;
+		selectMultiple.appendChild(option);
+		}
+	formContainer.appendChild(selectMultiple);
 }
 
 function imprimirEspacios(numeroEspacios) {
@@ -81,3 +144,45 @@ function imprimirEspacios(numeroEspacios) {
 		formContainer.appendChild(espacio);
 	}
 }
+
+function imprimirBotonCorregir() {
+	var botonCorregir = document.createElement("input");
+	botonCorregir.type = "submit";
+	botonCorregir.value = "Corregir";
+	formContainer.appendChild(botonCorregir);
+}
+
+//Correciones ************************************************************************
+
+function inicializar(){
+	document.getElementById('resultadosDiv').innerHTML = "";
+	nota=0.0;
+}
+
+function darRespuestaHtml(r){
+	var p = document.createElement("p");
+	var node = document.createTextNode(r);
+	p.appendChild(node);
+	document.getElementById('resultadosDiv').appendChild(p);
+}
+
+function presentarNota(){
+	darRespuestaHtml("Nota: "+nota+" puntos sobre 3");
+}
+
+window.onmousedown = function (e) {
+    var el = e.target;
+    if (el.tagName.toLowerCase() == 'option' && el.parentNode.hasAttribute('multiple')) {
+        e.preventDefault();
+
+        // toggle selection
+        if (el.hasAttribute('selected')) el.removeAttribute('selected');
+        else el.setAttribute('selected', '');
+
+        // hack to correct buggy behavior
+        var select = el.parentNode.cloneNode(true);
+        el.parentNode.parentNode.replaceChild(select, el.parentNode);
+    }
+}
+
+//Comprobar que se han introducido datos en el formulario
